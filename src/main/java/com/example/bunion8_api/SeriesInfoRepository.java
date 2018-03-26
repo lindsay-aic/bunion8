a864d294cdfcf42e7330548f3ddfb1a1fe5b8a46
lindsayaichinger <lindsayaichinger@192.168.39.128>
2018-03-26 15:27:24 -0700

    Added Repository for SeriesInfo, Rating, and Comment sections.

---
+++ src/main/java/com/example/bunion8_api/SeriesInfoRepository.java
@@ -0,0 +1,14 @@
+package com.example.bunion8_api;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface SeriesInfoRepository extends JpaRepository<SeriesInfo, Long> {
+
+	
+	
+	
+	
+}
